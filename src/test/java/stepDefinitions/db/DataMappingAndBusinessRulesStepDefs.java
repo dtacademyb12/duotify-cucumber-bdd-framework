7d0cc3f6caedbc17280397aab4c40151e27ee282
Duotech B4 <duotechb3@gmail.com>
2023-12-06 22:22:39 -0500

    business rules testing examples

--- src/test/java/stepDefinitions/db/DataMappingAndBusinessRulesStepDefs.java
+++ src/test/java/stepDefinitions/db/DataMappingAndBusinessRulesStepDefs.java
@@ -1,11 +1,14 @@
 package stepDefinitions.db;
 
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
 import stepDefinitions.SharedData;
 import utils.ConfigReader;
 import utils.DBUtils;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class DataMappingAndBusinessRulesStepDefs {
@@ -30,6 +33,59 @@ public class DataMappingAndBusinessRulesStepDefs {
 
 
 
+
+    }
+
+    List<String> actualColumnNames;
+    @When("I retrieve the column names from the {string} table")
+    public void iRetrieveTheColumnNamesFromTheTable(String table) {
+
+       actualColumnNames = DBUtils.getColumnNames("SELECT * from songs limit 1");
+
+
+    }
+
+    @Then("it should have the following")
+    public void itShouldHaveTheFollowing( List<String> expectedColumnNames) {
+
+        Assert.assertEquals(expectedColumnNames, actualColumnNames);
+
+    }
+    List<String> columnData;
+    @When("I retrieve the emails from {string} table")
+    public void iRetrieveTheEmailsFromTable(String table) {
+        List<List<Object>> resultAsListOfLists = DBUtils.getQueryResultAsListOfLists("SELECT email from " + table + "");
+        columnData = DBUtils.getColumnData(resultAsListOfLists, 0);
+
+
+
+    }
+
+    @Then("it should not contain duplicates")
+    public void itShouldNotContainDuplicates() {
+
+        // Find the duplicate (sort and compare adjacent element) using Java
+
+        Collections.sort(columnData);
+
+        boolean noDuplicate = true;
+        for (int i = 0; i < columnData.size()-1; i++) {
+
+            if(columnData.get(i).equals(columnData.get(i+1))){
+                noDuplicate = false;
+                break;
+            }
+        }
+
+
+        System.out.println(columnData);
+//        Assert.assertTrue(noDuplicate);
+        
+        
+        // Send a query that returns the table of duplicate emails
+
+        List<List<Object>> resultAsListOfLists = DBUtils.getQueryResultAsListOfLists("SELECT email, count(*) from users group by email having count(*)>1");
+        Assert.assertTrue(resultAsListOfLists.isEmpty()); // if the query returns any result, it should fail
 
     }
 }
--- src/test/resources/features/db/business-rules-and-data-mapping.feature
+++ src/test/resources/features/db/business-rules-and-data-mapping.feature
@@ -11,3 +11,23 @@ Feature: Data mapping and business rules scenarios
     And the user clicks on the "Your Music" link in the sidebar
     And the user creates a new playlist with random name
     Then the data should be mapped correctly in the corresponding table
+
+
+  Scenario: verify column names for songs table
+    When I retrieve the column names from the "songs" table
+    Then it should have the following
+      | id         |
+      | title      |
+      | artist     |
+      | album      |
+      | genre      |
+      | duration   |
+      | path       |
+      | albumOrder |
+      | plays      |
+
+
+  @business_rule @db_only
+  Scenario: verify email column for duplicates
+    When I retrieve the emails from "users" table
+    Then it should not contain duplicates
