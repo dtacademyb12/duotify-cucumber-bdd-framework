c38ea9aac2687daa1f8b2b6c7f11fe216f7f9149
Duotech B4 <duotechb3@gmail.com>
2024-01-10 21:04:48 -0500

    bearer token, jwt token

---
+++ src/test/java/pojos/LoginDetails.java
@@ -0,0 +1,18 @@
+package pojos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LoginDetails {
+
+    private String username;
+    private String password;
+}
--- src/test/java/utils/demo/RestAssuredAuthenticationSchemes.java
+++ src/test/java/utils/demo/RestAssuredAuthenticationSchemes.java
@@ -1,11 +1,17 @@
 package utils.demo;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.Assert;
 import org.junit.Test;
+import pojos.LoginDetails;
 
 import java.util.Base64;
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class RestAssuredAuthenticationSchemes {
 
@@ -44,6 +50,84 @@ public class RestAssuredAuthenticationSchemes {
                 statusCode(200);
 
 
+
+    }
+
+    @Test
+    public void bearerToken(){
+
+        RestAssured.baseURI = "https://api.github.com";
+
+        String branchName = "myBranch";
+        Map<String, String> body = Map.of("new_name", branchName );
+
+        String owner = "dtacademyb12";
+        String repo = "duotify-cucumber-bdd-framework";
+        String branch = "newBranch";
+        given().
+                header("Accept", "application/vnd.github+json").
+                header("X-GitHub-Api-Version", "2022-11-28").
+                header("Authorization", "Bearer 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").
+                pathParam("owner", owner).
+                pathParam("repo", repo).
+                pathParam("branch", branch).
+                body(body).
+
+        when().log().all().
+                post("/repos/{owner}/{repo}/branches/{branch}/rename").
+        then().log().all().statusCode(201).
+                body("name", equalTo(branchName));
+    }
+
+
+    @Test
+    public void JWTToken(){
+
+
+        RestAssured.baseURI = "http://duotify.us-east-2.elasticbeanstalk.com/api";
+        // Send a post request to login endpoint to obtain the JWT token for the next request
+
+       String jwtToken = given().
+
+                queryParam("api_key", "e82042a5f58f449c9d5a9e3cf5a3f43b").
+                body(LoginDetails.builder().
+                        username("duotech2023").
+                        password("duotech").build()).
+                when().log().all().
+                post("/login").
+                then().log().all().
+                statusCode(200).extract().jsonPath().getString("access_token");
+
+       //Send a get request to playlists endpoint by providing the obtained jwt token
+
+
+        JsonPath jsonPath = given().
+
+                queryParam("api_key", "e82042a5f58f449c9d5a9e3cf5a3f43b").
+                header("Authorization", jwtToken).
+                when().log().all().
+                get("/playlists").
+                then().log().all().
+                statusCode(200).extract().jsonPath();
+
+
+        Assert.assertEquals(200, jsonPath.getInt("status"));
+
+        List<Map<String, Object>> playlists = jsonPath.getList("playlists");
+
+//        System.out.println(playlists);
+
+        Map<String, Object> map = jsonPath.getMap("playlists[0]");
+
+        System.out.println(map);
+
+        String firstUSersid = jsonPath.getString("playlists[0].id");
+        System.out.println(firstUSersid);
+        List<String> ids = jsonPath.getList("playlists.id");
+
+        System.out.println(ids);
+
+        Assert.assertTrue(ids.contains("2414"));
 
     }
 
